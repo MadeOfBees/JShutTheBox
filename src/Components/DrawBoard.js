e402f6e7a0c8d7be60f9ec0c2c7228c9d0774256
Bee <xronsredstone@gmail.com>
2022-12-26 14:54:20 -0500

    a good spot to take a break

--- src/Components/DrawBoard.js
+++ src/Components/DrawBoard.js
@@ -1,24 +1,20 @@
 import * as React from 'react';
-import { Box, Button, Grid, Modal, Stack } from '@mui/material';
+import { Button } from '@mui/material';
 import PlayerLineup from './PlayerLineup';
 
 const DisplayBoard = ({ playerTotal }) => {
   const [players, setPlayers] = React.useState([]);
-
   const setupPlayers = () => {
     const playerArray = [];
     for (let i = 0; i < playerTotal; i++) {
-      playerArray.push({ playerNum: i + 1, score: 0, turn: false});
+      playerArray.push({ playerNum: i + 1, score: 0, turn: false });
     }
     playerArray[0].turn = true;
     setPlayers(playerArray);
     startGame();
   };
-
-  React.useEffect(() => {
-    setupPlayers();
-  }, []);
-
+  // eslint-disable-next-line
+  React.useEffect(() => { setupPlayers(); }, []);
   const startGame = () => {
     if (players.length > 0) {
       const newPlayers = [...players];
@@ -27,29 +23,29 @@ const DisplayBoard = ({ playerTotal }) => {
     }
   };
 
-  const handleRaiseScore = (playerIndex) => {
-    const newPlayers = [...players];
-    newPlayers[playerIndex].score += 1;
-    setPlayers(newPlayers);
-  };
-
-  const handlePassTurn = (playerIndex) => {
+  const nextTurn = (playerIndex) => {
     const newPlayers = [...players];
     newPlayers[playerIndex].turn = false;
     newPlayers[(playerIndex + 1) % playerTotal].turn = true;
     setPlayers(newPlayers);
   };
 
+  const rollDice = (playerIndex) => {
+    const newPlayers = [...players];
+    const dice = Math.floor(Math.random() * 6) + 1;
+    newPlayers[playerIndex].score += dice;
+    setPlayers(newPlayers);
+    nextTurn(playerIndex);
+  };
+
   return (
     <div>
       <PlayerLineup players={players} />
-
       {players.map((player, index) => {
         if (player.turn) {
           return (
             <div key={player.playerNum}>
-              <Button variant="contained" onClick={() => handleRaiseScore(index)}>Raise Player {player.playerNum}'s Score</Button>
-              <Button variant="contained" onClick={() => handlePassTurn(index)}>Pass Turn</Button>
+              <Button variant="contained" onClick={() => rollDice(index)}>Roll Dice</Button>
             </div>
           );
         }
