d229bb947cf0e37d48b74f4fc48a9193b4a20588
Bee <xronsredstone@gmail.com>
2022-12-27 02:09:04 -0500

    i'm happy with that

--- src/Components/PlayerLineup.js
+++ src/Components/PlayerLineup.js
@@ -4,14 +4,13 @@ import { useTheme } from '@mui/material/styles';
 
 const PlayerLineup = ({ players }) => {
   const theme = useTheme();
-
   return (
     <div className="main-div">
       <Grid container spacing={1} style={{ display: 'flex', justifyContent: 'center', marginTop: '2%' }}>
         {players.map(player => (
-          <Grid item xs={4} key={player.playerNum}>
-            <Box style={{ textAlign: 'center', color: player.turn ? theme.palette.primary.main : 'inherit', border: `.5REM solid ${theme.palette.mode === 'dark' ? "White" : "Black"}` }}>
-              <h5>Player {player.playerNum}</h5>
+          <Grid item xs={3} md={2} key={player.playerNum}>
+            <Box style={{textAlign: 'center', color: player.turn ? theme.palette.primary.main : 'inherit', border: `.5REM solid ${theme.palette.mode === 'dark' ? "White" : "Black"}` }}>
+              <h1>P{player.playerNum}</h1>
               <h5>Score: {player.score}</h5>
             </Box>
           </Grid>
