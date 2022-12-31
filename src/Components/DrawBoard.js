06057261be676af94673fd58fa43989be4657fd0
Bee <xronsredstone@gmail.com>
2022-12-31 15:59:22 -0500

    1.1.1.1

--- src/Components/DrawBoard.js
+++ src/Components/DrawBoard.js
@@ -6,7 +6,7 @@ import { Grid, Box, Modal, Button, Stack, Input } from '@mui/material';
 const toNineUp = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
-const DisplayBoard = ({ playerTotal }) => {
+const DisplayBoard = ({ playerTotal,style }) => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -24,7 +24,6 @@ const DisplayBoard = ({ playerTotal }) => {
   const [players, setPlayers] = React.useState([]);
   const [key, setKey] = React.useState(0);
   const [monoliths, setMonoliths] = React.useState(toNineUp);
-  const style = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, };
   const [userInput, setUserInput] = React.useState(0);
   const [refreshNum, setRefreshNum] = React.useState(0);
   const handleCloseWinnerModal = () => { setWinnerModal(false); restart(); }
